7bc7284a6123d5f742c635aff18993d527999cca
hadijahkyampeire <hadijah315@gmail.com>
2019-02-26 14:40:25 +0300

    change token key

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sms-management-api-mongo
+
+## Documentation
+[Docs](https://documenter.getpostman.com/view/2646235/S11GTLmL)
--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -1,5 +1,6 @@
 const get = require('lodash/get');
 const jwt = require('jsonwebtoken');
+const Contact = require('../models/contact');
 const { env } = require('../utils/helpers');
 
 const WhiteListedRoutes = ['/api/v1/authentication', '/api/v1/contacts/create'];
@@ -17,15 +18,22 @@ module.exports = (req, res, next) => {
   if (shouldByPassRequest(req)) {
     return next();
   }
-  const token = req.headers['x-access-token'];
+  const token = req.headers['authorization'];
+ 
   if (!token) return res.status(403).json({ error: 'No token provided.' });
-  jwt.verify(token, env('SECRET'), (err, decoded) => {
+  jwt.verify(token, env('SECRET'), async (err, decoded) => {
     if (req.headersSent) {
       return next(err);
     }
     if (err) return res.status(403).json({ error: 'Failed to authenticate token.' });
     // if everything is good, save to request for use in other routes
-    req.currentContact = get(jwt.decode(token), 'payload', {});
+    const payload = get(jwt.decode(token), 'payload', {});
+
+    const result = await Contact.findById(payload.contact_id);
+    if (!result) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+    req.currentContact = payload;
     return next();
   });
 };
